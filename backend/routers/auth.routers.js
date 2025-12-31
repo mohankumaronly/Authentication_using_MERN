dd04f54977bb9c35cb6de612c1f0805acd81788e
mohankumaronly <mohankumaronly81@gmail.com>
2025-12-31 21:33:21 +0530

    rate limiter is added for the auth

--- backend/routers/auth.routers.js
+++ backend/routers/auth.routers.js
@@ -4,11 +4,12 @@ const { register, login, logOut } = require('../controllers/auth.controller');
 const { registerSchema, loginSchema, resetPasswordSchema, forgotPasswordSchema } = require('../validators/auth.validators');
 const { resetPassword, forgotPassword } = require('../controllers/auth.forgot.controller');
 const tokenVerification = require('../middlewares/token.verification');
+const limiter = require('../middlewares/rate.limitor');
 
 const authRouter = express.Router();
 authRouter.post('/register', validate(registerSchema), register);
-authRouter.post('/login', validate(loginSchema), login);
-authRouter.post('/forgot-password', validate(forgotPasswordSchema), forgotPassword);
+authRouter.post('/login', validate(loginSchema), limiter, login);
+authRouter.post('/forgot-password', limiter, validate(forgotPasswordSchema), forgotPassword);
 authRouter.post('/reset-password/:token', validate(resetPasswordSchema), resetPassword);
 authRouter.post('/logout', logOut);
 // this router is just for checking
